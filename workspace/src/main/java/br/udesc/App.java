b94c70f05edd2222ec3de6386e045e4c0e8c3a42
Maciel Hogenn <maciel.hog@gmail.com>
2017-11-24 11:03:53 -0200

    Create project

---
+++ workspace/src/main/java/br/udesc/App.java
@@ -0,0 +1,34 @@
+package br.udesc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ResourceBundle;
+
+@RestController
+@EnableAutoConfiguration
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @RequestMapping(value = "/ping", consumes = "*/*", produces = "text/plain")
+    public String ping(){
+        return "OK";
+    }
+
+    @RequestMapping(value = "/version", consumes = "*/*", produces = "text/plain")
+    public String version(){
+        ResourceBundle rb = ResourceBundle.getBundle("version");
+        Object version = rb.getObject("version");
+        Object buildNumber = rb.getObject("build_number");
+
+        return String.join(" ", "Version", version.toString(), "build", buildNumber.toString());
+    }
+
+}
---
+++ workspace/src/main/java/br/udesc/security/AuthenticationFilter.java
@@ -0,0 +1,22 @@
+package br.udesc.security;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class AuthenticationFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
